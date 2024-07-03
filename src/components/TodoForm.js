ab652831085754cb664fa3e67e7f90d21c21e555
AdebiyiQudus <adebiyiqudus2018@gmail.com>
2024-07-03 05:01:04 +0100

    form

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -15,7 +15,8 @@ export const TodoForm = ({addTodoProp}) => {
       <input type="text" className="todo-input"
         value={value}
         placeholder="What is the task today?"
-         onChange={(e) => setValue(e.target.value)}/>
+         onChange={(e) => setValue(e.target.value)}
+         />
 
          <button type="submit" className="todo-btn">
           Add Task
